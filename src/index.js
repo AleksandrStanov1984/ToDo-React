c0d8691081d9ddd6c253bde0d6aa2bb4f719e13a
Aleksandr <aleksstanov1984@gmail.com>
2020-11-14 20:29:24 +0200

    New element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('hello World');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const ToDoList = () => {
+    const items = ['Learn React', 'Build Awesome App', 'Next'];
+    return(
+        <ul>
+            <li>{ items[0] }</li>
+            <li>{ items[1] }</li>
+            <li>{ items[2] }</li>
+        </ul>
+    );
+}
+
+const AppHeader = () => {
+    return <h1>Your ToDo List</h1>;
+}
+
+const SearchPanel = () => {
+    const Search = 'search...';
+    return <input placeholder={Search}/>;
+}
+
+const App = () => {
+    return(
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <ToDoList/>
+        </div>
+    );
+}
+
+
+
+ReactDom.render(<App/>, document.getElementById('root'));
